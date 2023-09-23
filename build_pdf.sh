3d160d535b39f788b1950a1bd2339bd592985a15
Jasper Chan <jasperchan515@gmail.com>
2023-09-23 15:37:40 -0700

    env: add nix development env

--- build_pdf.sh
+++ build_pdf.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 mkdir -p bin
-latexmk -f -pdf -output-directory=../bin -cd src/*.tex
-latexmk -f -pdf -output-directory=../bin -cd src/*.tex
-latexmk -f -c -output-directory=../bin -cd src/*.tex
+tectonic -k -o bin src/resume.tex
---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Environment for compiling documents";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: 
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            tectonic
+            git
+            git-lfs
+            ];
+          };
+      });
+}
