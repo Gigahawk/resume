d10a47b550fee05521bbfe7f36295618ddd1b300
Jasper Chan <jasperchan515@gmail.com>
2023-11-02 17:14:45 -0700

    transcript: add missing class and build in ci

--- build_pdf.sh
+++ build_pdf.sh
@@ -6,3 +6,7 @@ mkdir -p bin
 tectonic -k -o bin src/resume.tex
 exiftool -Author="$AUTHOR" -Revision="$BUILD_NAME" bin/resume.pdf
 rm bin/resume.pdf_original
+
+tectonic -k -o bin src/transcript.tex
+exiftool -Author="$AUTHOR" -Revision="$BUILD_NAME" bin/transcript.pdf
+rm bin/transcript.pdf_original
